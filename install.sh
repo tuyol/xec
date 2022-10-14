e72bd342bf2fe4fd0d79796b5aea4ac04460242a
tuyol <59181450+tuyol@users.noreply.github.com>
2022-10-14 08:57:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-sudo su
 apt update && apt upgrade -y && apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ -y
 git clone https://github.com/litecoincash-project/cpuminer-multi
 cd cpuminer-multi && ./build.sh
